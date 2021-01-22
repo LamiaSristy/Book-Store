9e5632898a2fc5941f5bc5f0b54bc87814d5c93f
lamiasristy <lamiasristy@gmail.com>
2021-01-22 10:19:41 +0100

    Added Linters, fixed the liters eroors and deploy for the latest.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>WelCome to Our Bookstore !!!</h1>       
+        <h1>WelCome to Our Bookstore !!!</h1>
       </header>
     </div>
   );

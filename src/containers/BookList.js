08231827475b9049f96e06e21b9247d885cea529
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-01-29 08:04:58 +0000

    Update mapDispatchToProps in the bookList component

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,22 +1,19 @@
-/*eslint-disable*/
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
-import CategoryFilter  from '../components/CategoryFilter';
+import CategoryFilter from '../components/CategoryFilter';
+import { removeBook } from '../actions/index';
 
 const mapDispatchToProps = dispatch => ({
   removeBook: book => {
-    dispatch({
-      ID: book.ID,
-      type: 'REMOVE_BOOK',
-    });
+    dispatch(removeBook(book.ID));
   },
 });
 
-const mapStateToProp = state => ({ 
+const mapStateToProp = state => ({
   books: state.bookReducer,
-  filter: state.filterReducer
+  filter: state.filterReducer,
 });
 
 const BookList = ({ books, removeBook, filter }) => {
@@ -26,23 +23,23 @@ const BookList = ({ books, removeBook, filter }) => {
 
   const renderBook = book => <Book key={book.ID} book={book} removeBook={handleRemoveBook} />;
 
-  const filteredBooks = filter === 'All' ? books : books.filter((book) => book.category === filter);
+  const filteredBooks = filter === 'All' ? books : books.filter(book => book.category === filter);
 
   return (
     <div>
-      <CategoryFilter  />
+      <CategoryFilter />
       <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>title </th>
-          <th>category</th>
-        </tr>
-      </thead>
-      <tbody>{filteredBooks.map(renderBook)}</tbody>
-    </table>  
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>title </th>
+            <th>category</th>
+          </tr>
+        </thead>
+        <tbody>{filteredBooks.map(renderBook)}</tbody>
+      </table>
     </div>
-  ); 
+  );
 };
 
 BookList.propTypes = {
